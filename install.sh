bed727d4871c264a2c7ed6113d70981259085b9b
Patrick Brideau <pbrideau@users.noreply.github.com>
2020-11-23 10:41:43 -0500

    bash: make bashrc shellcheck proof

--- install.sh
+++ install.sh
@@ -2,22 +2,22 @@
 
 
 # GIT
-mkdir -p $HOME/git
-git clone https://github.com/magicmonty/bash-git-prompt.git $HOME/git/
-ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
-ln -s $HOME/.dotfiles/git/gitignore $HOME/.gitignore
+mkdir -p "$HOME/git"
+git clone https://github.com/magicmonty/bash-git-prompt.git "$HOME/git/"
+ln -s "$HOME/.dotfiles/git/gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/.dotfiles/git/gitignore" "$HOME/.gitignore"
 
 # BASH
-mkdir $HOME/.bash_history_dir
-ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
+mkdir -p "$HOME/.bash_history_dir"
+ln -s "$HOME/.dotfiles/bashrc" "$HOME/.bashrc"
 
 # VIM
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
-ln -s $HOME/.dotfiles/vim/templates $HOME/.vim/templates
-ln -s $HOME/.dotfiles/vim/colors $HOME/.vim/colors
+curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
+ln -s "$HOME/.dotfiles/vim/templates" "$HOME/.vim/templates"
+ln -s "$HOME/.dotfiles/vim/colors" "$HOME/.vim/colors"
 vim +PlugInstall +qall
 echo "please install manually shellcheck software"
 
 # TMUX
-ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s "$HOME/.dotfiles/tmux.conf" "$HOME/.tmux.conf"
